50eba2cffee9a612519d12029a08c7eee180b571
Kaung Khant <thenotsorealkk@gmail.com>
2020-09-17 14:24:27 +0630

    Academic Year Implemented (MVC, Route, Migration, Factory, seed)

---
+++ ewsd_web/resources/views/academicyear/edit.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Academic Year Edit Form</title>
+</head>
+<body>
+    <form action="/academicyears/{{ $academic_year->id }}" method="POST">
+        @csrf
+        @method('PATCH')
+        Title : <input type="text" name="title" value="{{ $academic_year->title }}" required>
+        <br>
+        Description : <input type="text" name="description" value="{{ $academic_year->description }}" required>
+        <br>
+        Closure Date : <input type="date" name="closure_date" value="{{ $academic_year->closure_date }}" required>
+        <button type="submit">Save</button>
+    </form>
+
+    @if ($errors->any())
+
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+
+    @endif
+</body>
+</html>
