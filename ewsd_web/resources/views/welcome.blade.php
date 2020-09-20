cc3e327590ca183a219a2a934bab057ef046669b
ThuhtetCoder <59603198+ThuhtetCoder@users.noreply.github.com>
2020-09-20 14:59:17 +0630

    initial faculty

--- ewsd_web/resources/views/welcome.blade.php
+++ ewsd_web/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Faculty Part
                 </div>
 
                 <div class="links">
